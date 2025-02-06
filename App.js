6516a50657870b487953e300739275c7084a6c25
miia-mariastenius <n3stmi00@studens.oamk.fi>
2025-02-06 17:59:47 +0200

    Header components added

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import style from './styles/style';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { MyTheme, Styles } from './styles/style';
+import { Button, Divider, PaperProvider, Text } from 'react-native-paper';
 
 export default function App() {
   return (
-    <SafeAreaView style={style.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <PaperProvider theme={MyTheme}>
+      <SafeAreaView style={Styles.container}>
+        <View style={Styles.header}>
+        <Text variant='headlineLarge'>ADD EXERCISE</Text>
+        <Button mode='contained' style={Styles.saveButton}>Save</Button>
+        </View>
+        <Divider/>
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
--- styles/style.js
+++ styles/style.js
@@ -1,10 +1,24 @@
 import { StyleSheet } from "react-native";
+import { MD3LightTheme } from "react-native-paper";
 
-export default Styles = StyleSheet.create({
+export const MyTheme = {
+  ...MD3LightTheme,
+
+}
+
+export const Styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 10,
+  },
+  saveButton: {
+    width: 100,
+    
+  }
 })
